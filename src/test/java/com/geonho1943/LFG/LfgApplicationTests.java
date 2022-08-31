51e35332692f9571a4b0e955c70e93a074fe8711
geonho1943 <thsrjsgh02@naver.com>
2022-09-01 06:39:44 +0900

    Start Project

---
+++ src/test/java/com/geonho1943/LFG/LfgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geonho1943.LFG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LfgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
