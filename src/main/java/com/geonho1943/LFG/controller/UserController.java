c628799c1c3e050e693dfaf4cf27c061cebc2960
geonho1943 <thsrjsgh02@naver.com>
2022-09-12 01:03:37 +0900

    add join(issue)

--- src/main/java/com/geonho1943/LFG/controller/UserController.java
+++ src/main/java/com/geonho1943/LFG/controller/UserController.java
@@ -3,27 +3,38 @@ package com.geonho1943.LFG.controller;
 import com.geonho1943.LFG.model.UserModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserController {
+
     UserModel userModel = new UserModel();
-@GetMapping("/test")
-@ResponseBody
+
+    @GetMapping("/test")
+    @ResponseBody
     public String test(){
         return "test_ok";
     }
+
 
     @GetMapping("/user_lookup")
     @ResponseBody
     public String user_lookup(){
         return userModel.lookup();
     }
+    @GetMapping("/user_test")
+    @ResponseBody
+    public String url_test(@RequestParam("test")String test){
+        UserModel userModel = new UserModel();
+        return userModel.test(test);
+    }
+
 
     @GetMapping("/user_join")
-    @ResponseBody
-    public String user_join(){
-        return userModel.save("join is done!!");
+    public String user_join(@RequestParam("name")String name, @RequestParam("pw")String pw){
+        UserModel userModel = new UserModel();
+        return userModel.join(name,pw);
     }
 
 }
--- src/main/java/com/geonho1943/LFG/model/UserModel.java
+++ src/main/java/com/geonho1943/LFG/model/UserModel.java
@@ -1,18 +1,25 @@
 package com.geonho1943.LFG.model;
 
 import com.geonho1943.LFG.extraDB.ExtraDB;
+
 //DB에 user에 대한 저장,조회를 위한 기능들
 public class UserModel extends ExtraDB {
     private int idx=0;
 
-    public String save(String name){
-        setIdx(++idx);
+    public String test(String test){
+        setName(test);
+        return getName();
+    }
+
+    public String join(String name,String pw){
+        //setIdx(++idx);
         setName(name);
-        return "ok";
+        setPw(pw);
+        return getName()+" "+getPw()+"success!";
+        //!!!!
     }
     public String lookup(){
-        String asd = String.valueOf(getIdx())+getName();
-        return asd;
+        return String.valueOf(getIdx())+" "+getName();
     }
 
 }
