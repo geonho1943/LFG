36bbb8f727363c602ffb7ea437f20ef159c84ad9
geonho1943 <thsrjsgh02@naver.com>
2023-05-15 21:01:41 +0900

    Test: Doc main UI 테스트
    test

--- src/test/java/com/geonho1943/LFG/model/UserModelTest.java
+++ src/test/java/com/geonho1943/LFG/model/UserModelTest.java
@@ -4,16 +4,17 @@ import com.geonho1943.LFG.dto.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestPropertySource(locations = "/application-test.properties")
 @SpringBootTest
 class UserModelTest {
     @Autowired
@@ -22,28 +23,28 @@ class UserModelTest {
     @Autowired
     private UserRepository userRepository;
 
-    @Test
-    void join() throws SQLException {
-        //given
-        User user = new User();
-        user.setUser_id("test_id");
-        user.setUser_pw("test_pw");
-        user.setUser_name("test_name");
-
-        //when
-        User savedUser = userRepository.join(user);
-
-        //then
-        try (Connection conn = dataSource.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM lfg_user WHERE user_id = ?")) {
-            pstmt.setString(1, "test_id");
-            try (ResultSet rs = pstmt.executeQuery()) {
-                assertTrue(rs.next());
-                assertEquals(rs.getString("user_id"), savedUser.getUser_id());
-                assertEquals(rs.getString("user_name"), savedUser.getUser_name());
-            }
-        }
-    }
+//    @Test
+//    void join() throws SQLException {
+//        //given
+//        User user = new User();
+//        user.setUser_id("test_id");
+//        user.setUser_pw("test_pw");
+//        user.setUser_name("test_name");
+//
+//        //when
+//        User savedUser = userRepository.join(user);
+//
+//        //then
+//        try (Connection conn = dataSource.getConnection();
+//             PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM lfg_user WHERE user_id = ?")) {
+//            pstmt.setString(1, "test_id");
+//            try (ResultSet rs = pstmt.executeQuery()) {
+//                assertTrue(rs.next());
+//                assertEquals(rs.getString("user_id"), savedUser.getUser_id());
+//                assertEquals(rs.getString("user_name"), savedUser.getUser_name());
+//            }
+//        }
+//    }
 
     @Test
     void findAll() {
