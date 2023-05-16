581308e33d9b754e8db0777bdd81a8133ac6c8a8
geonho1943 <thsrjsgh02@naver.com>
2023-05-16 16:39:39 +0900

    Design: header디자인 변경
    초기 글자색은 눈에 띄는 흰색으로 설정후
    visited로 방문한 링크는 테마 색으로 변경되도록 설정함

--- src/test/java/com/geonho1943/LFG/model/UserModelTest.java
+++ src/test/java/com/geonho1943/LFG/model/UserModelTest.java
@@ -23,28 +23,29 @@ class UserModelTest {
     @Autowired
     private UserRepository userRepository;
 
-//    @Test
-//    void join() throws SQLException {
-//        //given
-//        User user = new User();
-//        user.setUser_id("test_id");
-//        user.setUser_pw("test_pw");
-//        user.setUser_name("test_name");
-//
-//        //when
-//        User savedUser = userRepository.join(user);
-//
-//        //then
-//        try (Connection conn = dataSource.getConnection();
-//             PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM lfg_user WHERE user_id = ?")) {
-//            pstmt.setString(1, "test_id");
-//            try (ResultSet rs = pstmt.executeQuery()) {
-//                assertTrue(rs.next());
-//                assertEquals(rs.getString("user_id"), savedUser.getUser_id());
-//                assertEquals(rs.getString("user_name"), savedUser.getUser_name());
-//            }
-//        }
-//    }
+    @Test
+    void join() throws SQLException {
+        //given
+        User user = new User();
+        user.setUser_id("test_id");
+        user.setUser_pw("test_pw");
+        user.setUser_name("test_name");
+
+        //when
+        User savedUser = userRepository.join(user);
+
+        //then
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM lfg_user WHERE user_id = ?")) {
+            pstmt.setString(1, "test_id");
+            try (ResultSet rs = pstmt.executeQuery()) {
+                assertTrue(rs.next());
+                assertEquals(rs.getString("user_id"), savedUser.getUser_id());
+                assertEquals(rs.getString("user_name"), savedUser.getUser_name());
+            }
+        }
+    }
+
 
     @Test
     void findAll() {
@@ -80,4 +81,5 @@ class UserModelTest {
         //when
         //then
     }
+
 }
