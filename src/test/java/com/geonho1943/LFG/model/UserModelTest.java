3db3d2b2b188744ce72ec7b9efc49dc614b6f738
geonho1943 <thsrjsgh02@naver.com>
2023-05-25 13:51:42 +0900

    Test: User에 관한 기능의 test case 작성
    user 서비스의 모든 기능
    join, role, login, auth, check, modify, sleep
    기능의 검증 코드 작성 완료

--- src/test/java/com/geonho1943/LFG/model/UserModelTest.java
+++ src/test/java/com/geonho1943/LFG/model/UserModelTest.java
@@ -48,13 +48,13 @@ class UserModelTest {
     }
     public void roleSetUp(){
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-        String createSchemaQuery = "CREATE SCHEMA IF NOT EXISTS LFGSERVICE;";
+//        String createSchemaQuery = "CREATE SCHEMA IF NOT EXISTS LFGSERVICE;";
         String createTableSql = "CREATE TABLE IF NOT EXISTS LFGservice.lfg_user_role"
         + " (`user_idx` int(11) NOT NULL,`user_role` int(11) NOT NULL,"
         + "PRIMARY KEY (`user_idx`), UNIQUE KEY `user_idx_UNIQUE` (`user_idx`))";
-        jdbcTemplate.execute(createSchemaQuery);
+//        jdbcTemplate.execute(createSchemaQuery);
         jdbcTemplate.execute(createTableSql);
-        LOGGER.info("H2 데이터베이스의 스키마,테이블 생성이 완료 되었습니다.");
+        LOGGER.info("H2: lfg_user_role Table이  생성이 완료 되었습니다.");
     }
     void customMethod(){
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
@@ -135,57 +135,102 @@ class UserModelTest {
     void role() throws SQLException {
         // Given
         roleSetUp();
+        customMethod();
         User user = new User();
-        user.setUser_idx(1);
+        user.setUser_id("test_id");
+        user.setUser_pw("test_pw");
+        user.setUser_name("test_name");
+        userRepository.join(user);
 
         // When
-        User assignedUser = userRepository.role(user);
+        userRepository.role(user);
+        userRepository.auth(user);
 
         // Then
-        assertEquals(user.getUser_idx(), assignedUser.getUser_idx());
-        LOGGER.info("역할 할당 검증이 완료되었습니다.");
+        assertEquals(2, user.getUser_role());
+        LOGGER.info("권한 할당 검증이 완료되었습니다.");
     }
 
     @Test
     void auth() {
         // given
+        roleSetUp();
+        customMethod();
+        User user = new User();
+        user.setUser_id("test_id2");//user_role 2 사용자 계정
+        user.setUser_pw("test_pw2");
+        user.setUser_name("test_name2");
+        userRepository.join(user);
+        userRepository.role(user);
+
         // when
+        userRepository.auth(user);
         // then
+        assertEquals(2,user.getUser_role());
+        LOGGER.info("권힌 조회 검증이 완료 되었습니다.");
     }
 
     @Test
     void check() {
         // given
+        customMethod();
+        User user = new User();
+        user.setUser_id("test_id_true");
+        User user1 = new User();
+        user1.setUser_id("test_id_false");
+        user1.setUser_pw("test_pw");
+        user1.setUser_name("test_name");
+        userRepository.join(user1);
+
         // when
+        boolean idcheck = userRepository.check(user);
+        boolean idFalseCheck = userRepository.check(user1);
+
         // then
+        assertEquals(true, idcheck);
+        LOGGER.info("사용 가능 ID 조회 검증을 성공 했습니다");
+        assertEquals(false, idFalseCheck);
+        LOGGER.info("사용 불가능 ID 조회 검증을 성공 했습니다");
     }
 
     @Test
     void modify() {
         // given
+        customMethod();
+        User user = new User();
+        user.setUser_id("test_id");
+        user.setUser_pw("test_pw");
+        user.setUser_name("test_name");
+        userRepository.join(user);
+
         // when
+        user.setUser_id("new_test_id");
+        user.setUser_pw("new_test_pw");
+        user.setUser_name("new_test_name");
+        userRepository.modify(user);
         // then
+        assertEquals("new_test_id", user.getUser_id());
+        assertEquals("new_test_pw", user.getUser_pw());
+        assertEquals("new_test_name", user.getUser_name());
     }
 
     @Test
     void sleep() throws SQLException {
         // Given
         User user = new User();
-        user.setUser_id("test_id1");
-        user.setUser_pw("test_pw1");
-        user.setUser_name("test_name1");
-
-        // 회원 등록
+        user.setUser_id("sleep_test_id");
+        user.setUser_pw("sleep_test_pw");
+        user.setUser_name("sleep_test_name");
         customMethod();
-        User registeredUser = userRepository.join(user);
+        userRepository.join(user);
 
         // When
-        userRepository.sleep(registeredUser);
+        userRepository.sleep(user);
 
         // Then
         try {
             userRepository.login(user);
-            fail("Expected IllegalArgumentException to be thrown");
+            fail("회원정보가 정상조회됨: 회원 정보가 정상적으로 삭제 되지 않았습니다");
         } catch (IllegalArgumentException e) {
             assertEquals("java.sql.SQLException: 회원정보를 다시 입력하세요", e.getMessage());
         }
